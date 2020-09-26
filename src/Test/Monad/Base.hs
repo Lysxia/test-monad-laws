02681bd027908c2a003b0b83d11908f948b25360
Koz Ross <koz.ross@retro-freedom.nz>
2020-09-27 12:22:58 +1300

    Initial description of MonadBase laws being checked

--- src/Test/Monad/Base.hs
+++ src/Test/Monad/Base.hs
@@ -12,12 +12,14 @@ import Test.QuickCheck.HigherOrder (Equation(..))
 
 import Test.Monad.Morph
 
+-- | > liftBase (return x) = return x
 liftBase_return
   :: forall m n a
   .  MonadBase n m
   => a -> Equation (m a)
 liftBase_return = returnHom @_ @m liftBase
 
+-- | > liftBase (m >>= k) = (liftBase m >>= liftBase k)
 liftBase_bind
   :: forall m n a b
   .  MonadBase n m
